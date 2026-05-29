301486e2e5114300d778811e2927fe640e035dc1
LuisSalas003 <salascampos003@hotmail.com>
2026-05-28 23:29:25 -0600

    ci: integración de build local de docker en el pipeline 2

--- AlfLab.Api/AlfLab.Api.Application/Interfaces/IAuditoriaRepository.cs
+++ AlfLab.Api/AlfLab.Api.Application/Interfaces/IAuditoriaRepository.cs
@@ -1,6 +1,5 @@
 using AlfLab.Api.Domain.Entities;
 using System.Threading.Tasks;
-// Prueba de pipeline independiente
 namespace AlfLab.Api.Application.Interfaces
 {
     public interface IAuditoriaRepository
