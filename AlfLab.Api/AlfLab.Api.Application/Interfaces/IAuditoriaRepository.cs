66400bd2d768f928068ed30a4b8607eb7b3a378b
LuisSalas003 <salascampos003@hotmail.com>
2026-05-28 22:27:41 -0600

    chore: prueba de trigger del pipeline de dotnet

--- AlfLab.Api/AlfLab.Api.Application/Interfaces/IAuditoriaRepository.cs
+++ AlfLab.Api/AlfLab.Api.Application/Interfaces/IAuditoriaRepository.cs
@@ -1,6 +1,6 @@
 using AlfLab.Api.Domain.Entities;
 using System.Threading.Tasks;
-
+// Prueba de pipeline independiente
 namespace AlfLab.Api.Application.Interfaces
 {
     public interface IAuditoriaRepository
